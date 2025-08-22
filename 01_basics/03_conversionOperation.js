24602cfa9c713a4b0475688afa0ba7c149733896
Jitendra Bodana <jitendrabodana@gmail.com>
2025-08-22 09:29:36 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,28 @@
-let score = "33"
+let score = "Jitendra"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let vlaueInNumber = Number(score);
+//console.log(typeof vlaueInNumber);
+//console.log(vlaueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number) but type will be number only
+// true => 1; false => 0
+
+let isLoggedIn = "jitendra";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "jitendra" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
