dc8369442acf91b9e9791879e600b52498b95c11
Jitendra Bodana <jitendrabodana@gmail.com>
2025-10-07 10:27:38 +0000

    Basics 02 Array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,11 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+
+myArr.push(6, 7);
+console.log(myArr);
+console.log(myArr[7]);
