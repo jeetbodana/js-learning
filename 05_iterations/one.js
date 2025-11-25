98d75050a39b2d13c2173f9ce4cb642f886e3ad6
Jitendra Bodana <jitendrabodana@gmail.com>
2025-11-25 11:23:51 +0000

    for loop concept

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5) {
+        // console.log("5 is lucky number")
+    }
+    // console.log(element);
+
+}
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value: ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j} and Outer loop ${i}`);
+//         console.log(i + '*' + j + ' =' + i*j );
+//     }
+// }
+
+
+let myArry = ["flash", "batman", "superman"]
+// console.log(myArry.length);
+for (let index = 0; index < myArry.length; index++) {
+    const element = myArry[index];
+    // console.log(element);
+}
+
+// break and continue 
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5) {
+//         console.log("Detected 5")
+//         break
+//     }
+//     console.log(`Value of i is ${index}`)
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5) {
+        console.log("Detected 5")
+        continue
+    }
+    console.log(`Value of i is ${index}`)
+}
+
