94fe3b019f67227a42df47070cc9d64e7fab7b66
Jitendra Bodana <jitendrabodana@gmail.com>
2025-11-25 11:48:04 +0000

    while and do while loop concept

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArry = ['flash', 'Batman', "superman"]
+
+// let arr = 0
+// while (arr < myArry.length) {
+//     console.log(`Value is ${myArry[arr]}`);
+//     arr = arr + 1;
+// }
+
+// Do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
